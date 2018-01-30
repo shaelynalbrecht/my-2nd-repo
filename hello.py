258be5fa9e00bcb89f4cb51dbdbb59ff7f4fb8bb
Kelsey Toporski <ktops@umich.edu>
2018-01-30 09:50:16 -0500

    my branch

--- hello.py
+++ hello.py
@@ -3,3 +3,7 @@ print("I was born in August")
 
 print("My name is Kelsey")
 print("I was born in June")
+print("My favorite food is mac and cheese, but I also really love sushi")
+print("My favorite type of music is alternative")
+print("My astrological sign is gemini")
+print("I am five feet tall with an extra inch")
