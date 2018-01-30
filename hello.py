e8de37bd5efb37f7cb6f76f0efd4b6e942e9369e
Shaelyn Albrecht <shaelyn@0587369623.wireless.umich.net>
2018-01-30 09:48:50 -0500

    added favorites

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 print("My name is Shaelyn")
 print("I was born in August")
+print("FAVES")
+print("food: pasta")
+print("music: pop & musicals")
+print("sign: Virgo")
 
 print("My name is Kelsey")
 print("I was born in June")
